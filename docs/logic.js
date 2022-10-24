14ef83de074d9bf61999b8b4ddc0a9a05fdc9a33
Josh Icard <joshicard@Joshs-MacBook-Pro.local>
2022-10-24 18:18:03 -0400

    current weather

--- docs/logic.js
+++ docs/logic.js
@@ -15,15 +15,10 @@ function fivedaydates() {
 
 async function getWeather() {
     console.log("The click worked");
-    //API Key
     var apikey = "f35f15ca2adc937d3e6afe2f22b4ba44";
-    //Search box input
     var searchInput = document.querySelector("#cityinput").value.trim();
     console.log(searchInput);
-    //API for coordinates
-    //https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
     https: var getCoordinates = "https://api.openweathermap.org/geo/1.0/direct?q="+searchInput+"&appid=f35f15ca2adc937d3e6afe2f22b4ba44&units=metric";
-    //Handle the response coordinates from API
     var coordinateData = await fetch(getCoordinates);
     var data1 = await coordinateData.json();
     console.log(data1);
@@ -31,11 +26,22 @@ async function getWeather() {
     console.log(coordinates);
     var {lat, lon} = coordinates;
     console.log(lat, lon);
-    https: var weatherDataUrl = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=f35f15ca2adc937d3e6afe2f22b4ba44";
-    var weatherDataResponse = await fetch(weatherDataUrl)
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        
+    https: var weatherDataUrl = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=f35f15ca2adc937d3e6afe2f22b4ba44&units=imperial";
+    var response = await fetch(weatherDataUrl)
+    var result = await response.json()
+    console.log(result);
+    var resultarray = Object.values(result);
+    console.log(resultarray);
+    var cityname = resultarray[11];
+    console.log(cityname);
+    console.log(result.weather[0].icon);
+    var {temp} = resultarray[3];
+    console.log(temp);
+    var {humidity} = resultarray[3];
+    console.log(humidity);
+    var {speed} = resultarray[5];
+    console.log(speed);
+
 }
 //Submit button onclick events
 var searchButton = document.querySelector("#submitbtn");
